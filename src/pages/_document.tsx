3c24c2a9483c60216da03a27ad3b2143c0915478
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-09-19 20:35:36 -0400

    primer paso en el curso

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+        {/* <link rel="sho" href="" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
